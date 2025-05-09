51887e45ecc5e03c2f0a9560203a329f0894d3e9
Letícia Farias <lf182000@gmail.com>
2025-05-09 16:10:55 -0300

    agora falta só o alternador

--- js/theme-toggle.js
+++ js/theme-toggle.js
@@ -20,5 +20,17 @@ class ThemeSwitcher {
 
         localStorage.setItem('theme', this.currentTheme)
     }
+    initEventListners(){
+        this.themeToggle.addEventListener('click', () => {
+            this.themeToggle()
+        })
+
+        window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => {
+            if (!localStorage.getItem('theme')) {
+                this.currentTheme = e.matches ? 'dark' : 'light'
+                this.initTheme()
+            }
+        })
+    }
 }
     
